b28d6cd8ed9bdbdaa0e5ef839d4b42c2e4e0ae05
EKA <46195766+EnesKeremAYDIN@users.noreply.github.com>
2021-04-10 16:53:14 +0300

    Delete mesaj-silinme-log.js

--- mesaj-silinme-log.js
+++
@@ -1,7 +0,0 @@
-client.on("messageDelete", messageDelete => {
-  messageDelete.channel.client.channels.cache
-    .get("loglarin-gonderilecegi-kanalin-idsi")
-    .send(
-      `:x: | **${messageDelete.author.tag} (${messageDelete.author.id})** şu mesajı sildi: ${messageDelete.content}`
-    );
-});
